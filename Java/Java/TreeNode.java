4eb0290758293f87c670600ae32f6715196fe9a1
Varnan Rathod <varnanrathod6@gmail.com>
2025-12-26 16:05:00 +0530

    TreeNode auxiliary class

---
+++ Java/Java/TreeNode.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class TreeNode {
+    public int value;
+    public ListNode left;
+    public ListNode right;
+
+    TreeNode() {
+    }
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
